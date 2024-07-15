215aac15ad4f49a5433e1a28c3780139e546f531
Bronito <BronitoNaturele@yandex.ru>
2024-07-15 18:42:04 +0700

    update project

--- pages/index_page.py
+++ pages/index_page.py
@@ -5,12 +5,13 @@ import re
 
 
 class IndexPage:
+    # переменная с локатором, который ищет элемент по альтернативному тексту
+    _Picture_Demo_Web_Shop_ = page.get_by_alt_text("Tricentis Demo Web Shop")
     # метод, который открывает нашу страницу
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
 
     # метод поиска картинки по тексту
-    def get_text_from_alt_text_on_main_page (self, page: Page) -> None:
-        locator = page.get_by_alt_text("Tricentis Demo Web Sho")
-        return locator
+    def get_text_from_alt_text_on_main_page(self, page: Page) -> None:
+        return page.locator(self._Picture_Demo_Web_Shop_).get_attribute('src')
         expect(locator).to_contain_text("Tricentis Demo Web Shop")
