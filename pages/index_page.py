5b728a32bb73ee016708ee938e3f94b969f0258e
Bronito <BronitoNaturele@yandex.ru>
2024-07-15 19:19:17 +0700

    update project

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,17 +1,17 @@
 from playwright.sync_api import Page
-from playwright.sync_api import expect
 import config
 import re
 
 
 class IndexPage:
-    # переменная с локатором, который ищет элемент по альтернативному тексту
-    _Picture_Demo_Web_Shop_ = page.get_by_alt_text("Tricentis Demo Web Shop")
+    # переменная с путём для картинки
+    _Picture_Demo_Web_Shop_ = "//body/div/div/div/div/a/img"
     # метод, который открывает нашу страницу
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
 
     # метод поиска картинки по тексту
-    def get_text_from_alt_text_on_main_page(self, page: Page) -> None:
-        return page.locator(self._Picture_Demo_Web_Shop_).get_attribute('src')
-        expect(locator).to_contain_text("Tricentis Demo Web Shop")
+    def get_element_and_text_from_attribute_on_main_page(self, page: Page) -> None:
+        # локатор, который ищет элемент по альтернативному тексту
+        return page.locator(self._Picture_Demo_Web_Shop_).get_attribute("alt")
+
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,9 +1,11 @@
 import pytest
 import pages
-import time
+
 
 class TestFooter:
 
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
         pages.index_page.open_index_page(page)
-        time.sleep(10)
+        pages.index_page.get_element_and_text_from_attribute_on_main_page(page)
+        actual_result = pages.index_page.get_element_and_text_from_attribute_on_main_page(page)
+        assert actual_result == "Tricentis Demo Web Shop"
