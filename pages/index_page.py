adaf96eaad60f6ea8a390e2da41e72bc8e8f055c
Bronito <BronitoNaturele@yandex.ru>
2024-07-15 18:32:38 +0700

    update project

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,8 +1,16 @@
 from playwright.sync_api import Page
+from playwright.sync_api import expect
 import config
+import re
 
 
 class IndexPage:
-
+    # метод, который открывает нашу страницу
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    # метод поиска картинки по тексту
+    def get_text_from_alt_text_on_main_page (self, page: Page) -> None:
+        locator = page.get_by_alt_text("Tricentis Demo Web Sho")
+        return locator
+        expect(locator).to_contain_text("Tricentis Demo Web Shop")
