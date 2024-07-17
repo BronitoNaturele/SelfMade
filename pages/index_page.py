16a90b6993b7c1707e309f296e2ca7f8b5af8375
Bronito <BronitoNaturele@yandex.ru>
2024-07-18 03:10:37 +0700

    update project

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,17 +1,29 @@
 from playwright.sync_api import Page
 import config
 import re
+import time
 
 
 class IndexPage:
     # переменная с путём для картинки
     _Picture_Demo_Web_Shop_ = "//body/div/div/div/div/a/img"
+    # переменная с сылкой, на картинке
+    _Link_Demo_Web_Shop_ =  '//body/div/div/div/div/a[@href="/"]'
     # метод, который открывает нашу страницу
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
 
     # метод поиска картинки по тексту
     def get_element_and_text_from_attribute_on_main_page(self, page: Page) -> None:
-        # локатор, который ищет элемент по альтернативному тексту
-        return page.locator(self._Picture_Demo_Web_Shop_).get_attribute("alt")
+        # локатор, который ищет элемент по атрибуту
+        locator_Picture_Demo_Web_Shop_ = page.locator(self._Picture_Demo_Web_Shop_).get_attribute('alt')
+        # возвращаяем, найденный выше атрибут в строку
+        return locator_Picture_Demo_Web_Shop_
 
+
+    def clicking_on_the_link(self, page: Page) -> None:
+        page.locator(self._Link_Demo_Web_Shop_).click(button="right")
+        time.sleep(2)
+        page.keyboard.press('ArrowDown')
+        time.sleep(2)
+        page.keyboard.press('Enter')
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -4,8 +4,19 @@ import pages
 
 class TestFooter:
 
-    def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
+    def test_user_should_be_able_to_open_main_page(self, page):
+        # открытие стартовой страницы
         pages.index_page.open_index_page(page)
+        # нахождение элемента по тексту в атрибуте, на стартовой странице
         pages.index_page.get_element_and_text_from_attribute_on_main_page(page)
+        # переменная с текстом из атрибута
         actual_result = pages.index_page.get_element_and_text_from_attribute_on_main_page(page)
+        # проверка соответствия текстов
         assert actual_result == "Tricentis Demo Web Shop"
+        print('Have a text')
+
+
+    def test_user_when_you_click_on_the_image_the_link_opens(self, page):
+        pages.index_page.open_index_page(page)
+        pages.index_page.clicking_on_the_link(page)
+
